53a45de798a410a592c98e26c3268744e2da8f1c
Cristian Rea <reacristian27@gmail.com>
2023-11-25 21:51:52 -0600

    primer commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Escuela</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+    <header>
+        <div class="navbar">
+            <div class="logo"><a href="#">Escuela</a></div>
+            <ul class="links">
+                <li><a href="hero">Home</a></li>
+                <li><a href="about">About</a></li>
+                <li><a href="service">Services</a></li>
+                <li><a href="contact">Contact</a></li>
+            </ul>
+
+            <a href="#" class ="action_btn"></a>
+        </div>
+    </header>
+</body>
+</html>
